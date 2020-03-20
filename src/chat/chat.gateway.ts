dccbb083b918ca59791833be891088ade6fb1d34
kyuuube <benchiu@yandex.com>
2020-03-20 18:05:51 +0800

    jenkins build

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -8,6 +8,8 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     @WebSocketServer() server;
     users: number = 0;
     info: any;
+    jobs: any[];
+    jobName: string;
 
     async handleConnection() {
 
@@ -30,14 +32,32 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     }
 
     @SubscribeMessage('chat')
-    async handleEvent(@ConnectedSocket() client: any, @MessageBody() data: string) {
+    async handleEvent(@ConnectedSocket() client: any, @MessageBody() data: any) {
         const logger = new Logger('chat');
         const event = 'chat';
         const jenkins = require('jenkins')({ baseUrl: `http://${process.env.JENKINS_USER}:${process.env.JENKINS_PASS}@${process.env.JENKINS_HOST}:8091`, crumbIssuer: true });
-        jenkins.info((err, data) => {
-        if (err) { throw err; }
-        this.server.emit(event, { name: 'jenkins', jobs: data.jobs , type: 'admin'});
-      });
+        if (data.text === '/list') {
+            jenkins.info((err, data) => {
+                if (err) { throw err; }
+                this.jobs = data.jobs;
+                this.server.emit(event, { name: 'jenkins', jobs: data.jobs , type: 'admin'});
+              });
+        }
+        if (data.text.includes('/build')) {
+          this.jobName =  data.text.slice(6, data.text.length);
+          this.jobs.forEach(i => {
+            logger.log(i.name, this.jobName);
+          });
+          const job = this.jobs.find(i => i.name === this.jobName);
+          logger.log(job);
+          jenkins.job.build({ name: job.url, parameters: { Branch: 'test' } }, (err, data) => {
+             logger.log(err, data);
+             if (err) {
+                return this.server.emit(event, { name: 'jenkins', text: 'err' });
+             }
+             this.server.emit(event, { name: 'jenkins', text: 'success' });
+          });
+      }
         this.server.emit(event, data);
     }
 }
